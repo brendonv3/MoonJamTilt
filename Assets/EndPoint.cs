26f94f1a592cc027ebb01d2b3803f1569aad961f
Brendon Steele <brendon@vision3creative.com>
2017-04-09 10:38:51 -0400

    Addedall the way up to level 5.

---
+++ Assets/EndPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour {
+
+	public ParticleSystem particle;
+
+	public void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("HitEndPoint");
+		particle.Play();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SimplePosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SimplePosition : MonoBehaviour {
+
+
+	public Vector3 deltaPosition;
+	public float time;
+	Vector3 cachedPosition;
+
+	void Start () {
+		//	CAche start posiiton
+		cachedPosition = this.transform.localPosition;
+
+		//	Start tween
+
+		Out ();
+	}
+
+	void Out(){
+		DOTween.Kill (this.transform);
+		this.transform.DOLocalMove ((cachedPosition + deltaPosition), time).SetEase(DG.Tweening.Ease.InOutQuad).OnComplete(()=>In());
+	}
+
+	void In(){
+		DOTween.Kill (this.transform);
+		this.transform.DOLocalMove ((cachedPosition), time).SetEase(DG.Tweening.Ease.InOutQuad).OnComplete(()=>Out());
+	}
+}
